570f22c1034b1970eb26fc672d271a23e74698f1
Stefan Kollmann <stefan.kollmann@hs-augsburg.de>
2016-11-21 14:56:39 +0100

    add an example application project

---
+++ example-application-project/src/main/java/com/github/kolleroot/gradle/kubernetes/example/application/ExampleApp.java
@@ -0,0 +1,10 @@
+package com.github.kolleroot.gradle.kubernetes.example.application;
+
+/**
+ * A hello world app
+ */
+public class ExampleApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
