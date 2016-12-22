a917e2fb1437975f76647258a38ffa1331c51013
Stefan Kollmann <stefan.kollmann@hs-augsburg.de>
2016-12-22 15:44:42 +0100

    Implement a KubernetesDelete task
    The task deletes a kubernetes object based on a config file.

--- plugin/src/integTest/groovy/com/github/kolleroot/gradle/kubernetes/integ/KubernetesTaskSpec.groovy
+++ plugin/src/integTest/groovy/com/github/kolleroot/gradle/kubernetes/integ/KubernetesTaskSpec.groovy
@@ -1,11 +1,16 @@
 package com.github.kolleroot.gradle.kubernetes.integ
 
 import com.github.kolleroot.gradle.kubernetes.testbase.GradleSpecification
+import io.fabric8.kubernetes.api.model.Pod
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
 import io.fabric8.kubernetes.client.KubernetesClient
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
+import io.fabric8.kubernetes.client.KubernetesClientException
+import io.fabric8.kubernetes.client.Watcher
 import spock.lang.Shared
+
+import java.security.SecureRandom
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 /**
  * Specify the kubernetes tasks and test the behavior against the default kubernetes cluster
@@ -13,15 +18,16 @@ import spock.lang.Shared
  * This test will create a new namespace for testing and clean it up afterwards.
  */
 class KubernetesTaskSpec extends GradleSpecification {
-    static final String NAMESPACE = 'kubernetes-tasks-test-namespace'
 
     static final String TEST_POD = 'test-pod'
+    static final String TEST_POD_FILE = 'test-pod.yaml'
 
-    @Rule
-    TemporaryFolder tmpFolder = new TemporaryFolder()
+    static final Random RANDOM = new SecureRandom()
 
     @Shared
     KubernetesClient kubernetesClient
+
+    String namespace = "kubernetes-tasks-test-namespace-${RANDOM.nextLong()}"
 
     def setupSpec() {
         kubernetesClient = new DefaultKubernetesClient()
@@ -30,13 +36,13 @@ class KubernetesTaskSpec extends GradleSpecification {
     def setup() {
         kubernetesClient.namespaces().createNew()
                 .withNewMetadata()
-                .withName(NAMESPACE)
+                .withName(namespace)
                 .endMetadata()
                 .done()
     }
 
     def cleanup() {
-        kubernetesClient.namespaces().withName(NAMESPACE).delete()
+        kubernetesClient.namespaces().withName(namespace).delete()
     }
 
     def cleanupSpec() {
@@ -45,22 +51,7 @@ class KubernetesTaskSpec extends GradleSpecification {
 
     def "create a simple pod from a yaml file"() {
         given: 'a config file'
-        buildFolder.newFile('test-pod.yaml') << """
-        apiVersion: v1
-        kind: Pod
-        metadata:
-            name: $TEST_POD
-            namespace: $NAMESPACE
-            labels:
-                state: test
-                app: simple
-        spec:
-            containers:
-              - name: a-container
-                image: openjdk:8-jdk-alpine
-                command:
-                  - "java -version"
-        """.stripIndent().trim()
+        buildFolder.newFile(TEST_POD_FILE) << testPodYaml
 
         and: 'a basic gradle project'
         buildFile << """
@@ -75,7 +66,7 @@ class KubernetesTaskSpec extends GradleSpecification {
         }
 
         task createPod(type: KubernetesCreate) {
-            configFile = file('test-pod.yaml')
+            configFile = file('$TEST_POD_FILE')
         }
         """.stripIndent().trim()
 
@@ -83,12 +74,83 @@ class KubernetesTaskSpec extends GradleSpecification {
         succeeds 'createPod'
 
         then: 'a pod will be created'
-        def pod = kubernetesClient.pods().inNamespace(NAMESPACE).withName(TEST_POD).get()
+        def pod = kubernetesClient.pods().inNamespace(namespace).withName(TEST_POD).get()
         pod != null
         pod.metadata.name == TEST_POD
         pod.metadata.labels == [state: 'test', app: 'simple']
         pod.spec.containers.size() == 1
         pod.spec.containers.get(0).name == 'a-container'
         pod.spec.containers.get(0).image == 'openjdk:8-jdk-alpine'
+    }
+
+    def "delete a simple pod from a yaml file"() {
+        given: 'a config file'
+        def testPodFile = buildFolder.newFile(TEST_POD_FILE) << testPodYaml
+
+        and: 'a pod on the cluster'
+        kubernetesClient.load(testPodFile.newInputStream()).createOrReplace()
+
+        and: 'a watcher on the pod listening for the delete event'
+        final DELETE_LATCH = new CountDownLatch(1)
+
+        kubernetesClient.pods().inNamespace(namespace).withName(TEST_POD).watch(new Watcher<Pod>() {
+            @Override
+            void eventReceived(Watcher.Action action, Pod resource) {
+                if (action == Watcher.Action.DELETED) {
+                    DELETE_LATCH.countDown()
+                }
+            }
+
+            @Override
+            void onClose(KubernetesClientException cause) {
+
+            }
+        })
+
+        and: 'a basic gradle project deleting the pod'
+        buildFile << """
+        import com.github.kolleroot.gradle.kubernetes.task.KubernetesDelete
+
+        plugins {
+            id 'com.github.kolleroot.gradle.kubernetes'
+        }
+
+        repositories {
+            jcenter()
+        }
+
+        task deletePod(type: KubernetesDelete) {
+            configFile = file('$TEST_POD_FILE')
+        }
+        """.stripIndent().trim()
+
+        when: 'gradle runs the delete task'
+        succeeds 'deletePod'
+
+        then: 'wait till the pod is deleted and verify that it is realy deleted'
+        DELETE_LATCH.await(10, TimeUnit.SECONDS)
+
+        def pod = kubernetesClient.pods().inNamespace(namespace).withName(TEST_POD).get()
+        pod == null
+    }
+
+    private String getTestPodYaml() {
+        """
+        apiVersion: v1
+        kind: Pod
+        metadata:
+            name: $TEST_POD
+            namespace: $namespace
+            labels:
+                state: test
+                app: simple
+        spec:
+            containers:
+              - name: a-container
+                image: openjdk:8-jdk-alpine
+                command:
+                  - "sh"
+                  - "-c \\"java -version; while true; do echo -n '. '; sleep 1; done\\""
+        """.stripIndent().trim()
     }
 }
--- plugin/src/main/groovy/com/github/kolleroot/gradle/kubernetes/task/KubernetesTasks.groovy
+++ plugin/src/main/groovy/com/github/kolleroot/gradle/kubernetes/task/KubernetesTasks.groovy
@@ -1,7 +1,9 @@
 package com.github.kolleroot.gradle.kubernetes.task
 
+import io.fabric8.kubernetes.api.model.HasMetadata
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
 import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.dsl.NamespaceVisitFromServerGetDeleteRecreateApplicable
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
@@ -20,13 +22,31 @@ abstract class KubernetesTask extends DefaultTask {
     protected abstract void kubernetesAction(KubernetesClient client)
 }
 
-class KubernetesCreate extends KubernetesTask {
-
+abstract class KubernetesLoadedTask extends KubernetesTask {
     @InputFile
     File configFile
 
     @Override
     protected void kubernetesAction(KubernetesClient client) {
-        client.load(configFile.newInputStream()).createOrReplace()
+        kubernetesLoadedAction(client.load(configFile.newInputStream()))
+    }
+
+    abstract void kubernetesLoadedAction(
+            NamespaceVisitFromServerGetDeleteRecreateApplicable<List<HasMetadata>, Boolean> loaded)
+}
+
+class KubernetesCreate extends KubernetesLoadedTask {
+    @Override
+    void kubernetesLoadedAction(
+            NamespaceVisitFromServerGetDeleteRecreateApplicable<List<HasMetadata>, Boolean> loaded) {
+        loaded.createOrReplace()
+    }
+}
+
+class KubernetesDelete extends KubernetesLoadedTask {
+    @Override
+    void kubernetesLoadedAction(
+            NamespaceVisitFromServerGetDeleteRecreateApplicable<List<HasMetadata>, Boolean> loaded) {
+        loaded.delete()
     }
 }
